88b4d610f66da5ede73ce5399bf60f6611ef8439
zezhou.zhou <zezhou.zhou@cootek.cn>
2016-03-30 15:10:10 +0800

    create db table android

--- AdminSystem/app/src/main/java/com/wirelessorder/adminsystem/activity/MainActivity.java
+++ AdminSystem/app/src/main/java/com/wirelessorder/adminsystem/activity/MainActivity.java
@@ -1,18 +1,40 @@
 package com.wirelessorder.adminsystem.activity;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.wirelessorder.adminsystem.R;
+import com.wirelessorder.adminsystem.utils.DataBaseHelper;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends ActionBarActivity {
+    private DataBaseHelper mDataBaseHelper;
+    private SQLiteDatabase mDB;
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mDataBaseHelper = new DataBaseHelper(this);
+        mDB = mDataBaseHelper.getWritableDatabase();
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(mToolbar);
+        //设置抽屉DrawerLayout
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
+                R.string.drawer_open, R.string.drawer_close);
+        mDrawerToggle.syncState();//初始化状态
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 
 }
--- AdminSystem/app/src/main/java/com/wirelessorder/adminsystem/utils/DataBaseHelper.java
+++ AdminSystem/app/src/main/java/com/wirelessorder/adminsystem/utils/DataBaseHelper.java
@@ -11,13 +11,59 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public static final String DB_NAME = "mydb.db";
     public static final int DB_VERSION = 1;
 
+    public static final String CREATE_TABLE_ADMIN = "CREATE TABLE t_admin (" +
+            "  admin_id integer primary key autoincrement," +
+            "  admin_name varchar(255)," +
+            "  admin_password varchar(255)" +
+            ");";
+
+    public static final String CREATE_TABLE_MEAL = "CREATE TABLE t_meal (" +
+            "  meal_id integer primary key autoincrement," +
+            "  meal_type_id integer ," +
+            "  meal_name varchar(255) ," +
+            "  meal_price double ," +
+            "  meal_image varchar(255) ," +
+            "  meal_info varchar(255)" +
+            ");";
+
+    public static final String CREATE_TABLE_MEAL_TYPE = "CREATE TABLE t_meal_type (" +
+            "  meal_type_id integer primary key autoincrement," +
+            "  meal_type_name varchar(255) " +
+            ");";
+
+    public static final String CREATE_TABLE_ORDER = "CREATE TABLE t_order (" +
+            "  order_id integer primary key autoincrement," +
+            "  user_id integer ," +
+            "  order_user_amount integer ," +
+            "  order_sum double ," +
+            "  order_date varchar(255) " +
+            ");";
+
+    public static final String CREATE_TABLE_ORDER_DETAIL = "CREATE TABLE t_order_detail (" +
+            "  order_id integer ," +
+            "  meal_id integer ," +
+            "  meal_amount integer" +
+            ");";
+
+    public static final String CREATE_TABLE_USER = "CREATE TABLE t_user (" +
+            "  user_id integer  primary key autoincrement," +
+            "  user_password varchar(255) ," +
+            "  user_name varchar(255) ," +
+            "  user_phone varchar(255)" +
+            ");";
+
     public DataBaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createUser = "";
+        db.execSQL(CREATE_TABLE_ADMIN);
+        db.execSQL(CREATE_TABLE_MEAL);
+        db.execSQL(CREATE_TABLE_MEAL_TYPE);
+        db.execSQL(CREATE_TABLE_ORDER);
+        db.execSQL(CREATE_TABLE_ORDER_DETAIL);
+        db.execSQL(CREATE_TABLE_USER);
     }
 
     @Override
